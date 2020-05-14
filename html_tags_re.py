5e6c766493e0a86dfedde1dadbba1b71ddd9f3ea
ayushpayasi <ayushpayasi@users.noreply.github.com>
2020-05-14 23:19:07 +0530

    html_tags_re.py

--- html_tags_re.py
+++ html_tags_re.py
@@ -1,13 +1,18 @@
-# this file finds all the tags present in an html document
+# this file finds all the tags present in an html document 
 tc = int(input())
 import re
 li=[]
 li2=[]
+
 for i in range(tc):
     stri =input()
     result = re.findall(r"(?<=\<)\w*",stri)
     li.extend(list(result))
 li=list(set(li))
+
+# this list li contains all the tags
+# below is the only for representation (simply use print(li) instead) 
+
 for i in li:
     if i !="":
         li2.append(i)
